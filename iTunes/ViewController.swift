f7508d49bbd5ce1ee6552c6b5b2062ee30ae1879
saengle <100755761+saengle@users.noreply.github.com>
2024-08-11 18:20:47 +0900

    Initial Commit

---
+++ iTunes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunes
+//
+//  Created by 쌩 on 8/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+
+}
+
