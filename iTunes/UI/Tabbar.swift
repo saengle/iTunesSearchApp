ff079c30f9c02ccc7ad1ae1fb000b242b2d4107c
saengle <100755761+saengle@users.noreply.github.com>
2024-08-11 18:29:45 +0900

    [Add] Tabbar

---
+++ iTunes/UI/Tabbar.swift
@@ -0,0 +1,25 @@
+//
+//  Tabbar.swift
+//  iTunes
+//
+//  Created by 쌩 on 8/11/24.
+//
+
+import UIKit
+
+import Kingfisher
+
+// MARK:   Main Tabbar
+final class TabbarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let search = SearchViewController()
+        let searchNav = UINavigationController(rootViewController: search)
+        searchNav.tabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "magnifyingglass"), selectedImage: UIImage(systemName: "magnifyingglass.fill"))
+        
+        
+        setViewControllers([ searchNav], animated: true)
+    }
+}
